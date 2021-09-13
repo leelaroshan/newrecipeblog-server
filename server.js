0336b7ca869e8554584af6e8ca9f1ced32f238a4
Leelavathi Gade <leelaroshangade@gmail.com>
2021-09-13 16:49:20 +0200

    final view

---
+++ server.js
@@ -0,0 +1,27 @@
+
+require('dotenv').config();
+require('colors');
+const express = require('express');
+
+
+const recipes = require('./api/recipes');
+
+const connectDB = require('./dbinit');
+const server = express();
+const PORT = process.env.PORT || 5000;
+
+
+connectDB();
+
+server.use(express.json())
+
+server.get('/', (req, res) => res.send('Welcome to Node with Postgres!'));
+server.use('/recipes', recipes);
+
+
+
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+
+
